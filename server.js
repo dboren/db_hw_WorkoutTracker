80dad72373d2278bad3885a535bc109803f8b75a
dboren <danielboren@att.net>
2021-04-29 21:48:46 -0500

    finished writing server file and added models folder

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+    useNewUrlParser: true,
+    useFinadAndModify: false
+});
+
+app.use("routes");
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
